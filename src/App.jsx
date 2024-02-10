8fb8a04ae297b8536c305f423e625042dc315e42
Bhupesh-Thakur <153527966+thakurbhupesh785@users.noreply.github.com>
2024-02-10 19:38:11 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+// onchange = event handler used primarily with form elements
+// eg- <input>,<textarea>,<select>,<radio>
+// triggers a function every time the value of input changes
+
+import ColorPicker from "./ColorPicker.jsx"
+
+
+function App() {
+
+  
+  return(
+   <>
+   <ColorPicker/>
+   </>
+  )
+ 
+}
+
+export default App
